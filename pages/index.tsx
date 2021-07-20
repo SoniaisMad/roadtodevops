1e78697411d7cf38d2591419cfa319313d5600d8
sonia manoubi <soniamanoubi@MacBook-Pro-de-sonia.local>
2021-07-20 09:24:23 +0200

    road to devops

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,40 +13,39 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Road to Devops
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          My journey from backend developer to Devops
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://crunchcrunch.me/ressources-that-helped-me-get-my-aws-cda-certification" className={styles.card}>
+            <h2>AWS Certification &rarr;</h2>
+            <p>AWS CDA</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://github.com/SoniaisMad/100DaysOfKubernetesJourney" className={styles.card}>
+            <h2>#100DaysOfKubernetes &rarr;</h2>
+            <p>Learning Kubernetes from zero</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="/reading"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Read &rarr;</h2>
+            <p>Resources that i am reading</p>
+          </a>
+
+          <a
+            href="https://twitter.com/zoun"
+            className={styles.card}
+          >
+            <h2>Where to follow me &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Follow me and share your road also!
             </p>
           </a>
         </div>
@@ -58,10 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Made with love and Next.js
         </a>
       </footer>
     </div>
---
+++ pages/reading.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function Reading() {
+  return (
+    <div className={styles.container}>
+      I am still making my list :)
+    </div>
+  )
+}
